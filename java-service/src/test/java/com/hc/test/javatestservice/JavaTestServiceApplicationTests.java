841a8a0a5d470848355cf00092a76a63b3f8c84f
黄超 <chao.huang@five-star.cn>
2022-09-01 17:39:37 +0800

    黄超的测试工程

---
+++ java-service/src/test/java/com/hc/test/javatestservice/JavaTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.test.javatestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
